5fa961709e6c4c11bf02eefa802c0f6d6670ba93
uiandwe <uiandwe@gmail.com>
2016-08-14 20:47:23 +0900

    get query and runtime

--- post/testMiddle.py
+++ post/testMiddle.py
@@ -1,4 +1,8 @@
 __author__ = 'uiandwe'
+from django.conf import settings
+from django.db import connection
+from django.template import Template, Context
+
 
 
 class SimpleMiddleware(object):
@@ -23,5 +27,18 @@ class SimpleMiddleware(object):
 
 
     def process_response(self, request, response):
+
+        if settings.DEBUG and connection.queries:
+            execution_sql_time = sum([float(q['time']) for q in connection.queries])
+            t = Template(
+            """
+            {{nb_sql}} requet{{nb_sql|pluralize:"e,es"}} en {{execution_sql_time}} second{{execution_sql_time|pluralize:"e,es"}}:
+            {% for sql in sql_log %}
+            [{{forloop.counter}}] {{sql.time}}s: {{sql.sql|safe}}
+            {% endfor %}
+            """)
+            print("---------------")
+            print(t.render(Context({'sql_log':connection.queries,'nb_sql':len(connection.queries),'execution_sql_time':execution_sql_time})))
+            print("---------------")
         print(request, response)
         return response
